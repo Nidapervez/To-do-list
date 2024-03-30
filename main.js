67880f20d5dfc8be25c523e2ec05f2e3189a4901
nidapervez <nnpervez333@gmail.com>
2024-03-30 02:19:40 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addtask = await inquirer.prompt([
+        {
+            type: "input",
+            name: "todo",
+            message: "What do you want to add in your todos?",
+        }, {
+            type: "confirm",
+            name: "addmore",
+            message: "Do you want to add more?",
+            default: "false"
+        }
+    ]);
+    todos.push(addtask.todo);
+    condition = addtask.addmore;
+    console.log(todos);
+}
